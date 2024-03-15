9ca34d78948b367c3efbade77ffebfc603e88b49
Michael Hobden <michael.hobden03@gmail.com>
2024-03-15 14:23:11 +0000

    Working on main body of the code
    galois multiplication is a nightmare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import math
 
 sbox = [[0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76],
         [0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0,
@@ -124,21 +123,130 @@ def add_padding(plaintext, block_size=16):
 def convert_text_to_blocks(text):
     text = text.encode('utf-8')
     text = add_padding(text)
-    return [text[x*16:(x+1)*16] for x in range(len(text)//16)]
+    text_blocks = [text[i:i+16] for i in range(0, len(text), 16)]
+    formatted_blocks = []
+
+    for block in text_blocks:
+        formatted_block = []
+        for i in range(0, len(block), 4):
+            formatted_word = []
+            for j in range(4):
+                formatted_word.append(format(block[i+j], '02x'))
+            formatted_block.append(formatted_word)
+        formatted_blocks.append(formatted_block)
+
+    return formatted_blocks
 
 
 def sub_bytes(block):
     for i in range(4):
-        for j in range(4):
-            block[i][j] = sbox[block[i][j]]
+        block[i] = Subword_Function(block[i])
     return block
+
+
+def shift_rows(block):
+    b = block
+    b[1][0], b[1][1], b[1][2], b[1][3] = b[1][1], b[1][2], b[1][3], b[1][0]
+    b[2][0], b[2][1], b[2][2], b[2][3] = b[2][2], b[2][3], b[2][0], b[2][1]
+    b[3][0], b[3][1], b[3][2], b[3][3] = b[3][3], b[3][0], b[3][1], b[3][2]
+    return b
+
+
+def multiply_galois(a, b):
+    a = bin(a)[2:].zfill(8)
+    b = bin(int(b, 16))[2:].zfill(8)
+
+    result = 0
+
+    for i in range(8):
+        if (int(b, 2) & 1) == 1:
+            result ^= int(a, 2)
+        hi_bit_set = int(a, 2) & 0x80
+        a = bin((int(a, 2) << 1) & 0xFF)[2:].zfill(8)
+        if hi_bit_set == 0x80:
+            a = bin(int(a, 2) ^ 0x1B)[2:].zfill(8)
+        b = bin(int(b, 2) >> 1)[2:].zfill(8)
+
+    # Convert the result back to hexadecimal
+    return hex(result)
+
+
+print(multiply_galois(0x1B, 'B5'))
+
+
+def mix_columns(block):
+    for i in range(4):
+        s0 = block[0][i]
+        s1 = block[1][i]
+        s2 = block[2][i]
+        s3 = block[3][i]
+
+        # transformation matrix
+        '''
+        02, 03, 01, 01
+        01, 02, 03, 01
+        01, 01, 02, 03
+        03, 01, 01, 02
+        '''
+
+        s0_mult_02 = multiply_galois(0x02, s0)
+        s1_mult_03 = multiply_galois(0x03, s1)
+        s1_mult_02 = multiply_galois(0x02, s1)
+        s2_mult_03 = multiply_galois(0x03, s2)
+        s2_mult_02 = multiply_galois(0x02, s2)
+        s3_mult_03 = multiply_galois(0x03, s3)
+        s0_mult_03 = multiply_galois(0x03, s0)
+        s3_mult_02 = multiply_galois(0x02, s3)
+        s2_mult_01 = s2
+        s3_mult_01 = s3
+        s1_mult_01 = s1
+        s0_mult_01 = s0
+
+        block[0][i] = hex(int(s0_mult_02, 16) ^ int(
+            s1_mult_03, 16) ^ int(s2_mult_01, 16) ^ int(s3_mult_01, 16))[2:]
+        block[1][i] = hex(int(s0_mult_01, 16) ^ int(
+            s1_mult_02, 16) ^ int(s2_mult_03, 16) ^ int(s3_mult_01, 16))[2:]
+        block[2][i] = hex(int(s0_mult_01, 16) ^ int(
+            s1_mult_01, 16) ^ int(s2_mult_02, 16) ^ int(s3_mult_03, 16))[2:]
+        block[3][i] = hex(int(s0_mult_03, 16) ^ int(
+            s1_mult_01, 16) ^ int(s2_mult_01, 16) ^ int(s3_mult_02, 16))[2:]
+
+        print(block)
+
+    return block
+
+
+def add_round_key(blocked_text, key):
+    for i in range(len(blocked_text)):
+        working_block = blocked_text[i]
+        complete_bytes = []
+        for byte in working_block:
+            print(byte)
+            byte = int(''.join(byte), 16)
+            byte ^= int(''.join(key), 16)
+            hex_c = format(byte, '08x')
+            hex_c = [hex_c[j:j+2]
+                     for j in range(0, len(hex_c), 2)]
+            complete_bytes.append(hex_c)
+        blocked_text[i] = complete_bytes
+    return blocked_text
 
 
 def encrypt_text(text, key):
     key_list = key_expansion(key)
     blocked_text = convert_text_to_blocks(text)
 
-    print(blocked_text)
+    # setup round
+    key = key_list[0]
+    blocked_text = add_round_key(blocked_text, key)
+
+    # main_rounds
+    for key in key_list[1:-1]:
+        print("New Round")
+        blocked_text = [sub_bytes(block) for block in blocked_text]
+        blocked_text = [shift_rows(block) for block in blocked_text]
+        blocked_text = [mix_columns(block) for block in blocked_text]
+    # main rounds
 
 
 key = ["0f", "15", "71", "c9", "47", "d9", "e8", "59",
@@ -146,4 +254,4 @@ key = ["0f", "15", "71", "c9", "47", "d9", "e8", "59",
 
 text = 'michael is super cool and this aes-256 encryption is going to work'
 
-encrypt_text(text, key)
+# encrypt_text(text, key)
